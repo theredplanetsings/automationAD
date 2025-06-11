1d60689f8eebb82da9f3b3985a1371d3301b294d
001 <152649123+theredplanetsings@users.noreply.github.com>
2025-06-11 10:57:53 -0400

    Update userCreationFromCSV.ps1

--- userCreationFromCSV.ps1
+++ userCreationFromCSV.ps1
@@ -1,30 +1,31 @@
 Import-Module ActiveDirectory
 # global constants
-$domainname = "paradigmcos.com"
+$domainname = "paradigmcos.local"
 # default password for new users
 $defaultpassword = ConvertTo-SecureString "Password123@" -AsPlainText -Force
-# import CSV; ensure your CSV file has the correct headers
+# import CSV; ensure your CSV file has the correct headers such as:
+# FirstName, LastName, Office, Address, PostalCode, Telephone, Email, MailNickname, ProxyAddresses, JobTitle, Department, Company, Groups
 $users = Import-Csv -Path "C:\Users\$env:username\Downloads\it-tools\INSERT_FILE_NAME.csv"
 
 foreach ($user in $users) {
     # produces username: first letter of first name + last name, all in lowercase
     $username = "$($user.FirstName.Substring(0,1))$($user.LastName)".ToLower()
     $userPrincipalName = "$username@$domainname"
+    $fullName = "$($user.FirstName) $($user.LastName)"
 
-    # creating new user in the AD
+    # creating new user in the domain's Users OU
     New-ADUser `
-        -Name "$($user.FirstName) $($user.LastName)" `
+        -Name $fullName `
         -GivenName $user.FirstName `
         -Surname $user.LastName `
         -SamAccountName $username `
         -UserPrincipalName $userPrincipalName `
-        -Path "OU=Users,DC=domain,DC=com" `  # Update this path as needed.
+        -Path "OU=Users,DC=paradigmcos,DC=local" `
         -AccountPassword $defaultpassword `
         -Enabled $true `
         -PassThru
 
-    # assigning specified security groups to the user
-    # expecting comma separated values in "groups" column of CSV
+    # assigning specified security groups to the user (CSV Groups column should be comma separated)
     if ($user.Groups) {
         $groupList = $user.Groups -split ","
         foreach ($grp in $groupList) {
@@ -32,16 +33,19 @@ foreach ($user in $users) {
         }
     }
 
-    # setting additional user properties
+    # setting additional user properties for new user
     Set-ADUser $username `
-        -Office $user.Office `
-        -OfficePhone $user.Telephone `
-        -EmailAddress $user.Email `
         -Replace @{
-            mailNickname = $user.MailNickname
-            proxyAddresses = ($user.ProxyAddresses -split ",") # supports multiple SMTP addresses if needed
-            title = $user.JobTitle
-            department = $user.Department
-            company = $user.Company
+            physicalDeliveryOfficeName = $user.Office
+            streetAddress              = $user.Address
+            postalCode                 = $user.PostalCode
+            telephoneNumber            = $user.Telephone
+            mail                       = $user.Email
+            mailNickname               = $user.MailNickname
+            proxyAddresses             = ($user.ProxyAddresses -split ",")
+            title                      = $user.JobTitle
+            department                 = $user.Department
+            company                    = $user.Company
+            name                       = $fullName
         }
 }
