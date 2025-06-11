086b58a3899209283f3628c357caafdb5a69173d
001 <152649123+theredplanetsings@users.noreply.github.com>
2025-06-11 10:57:42 -0400

    Update userCreationFromInput.ps1

--- userCreationFromInput.ps1
+++ userCreationFromInput.ps1
@@ -1,12 +1,15 @@
 Import-Module ActiveDirectory
-#constants
-$domainname = "paradigmcos.com"
+
+# constants
+$domainname = "paradigmcos.local"
 $defaultpassword = ConvertTo-SecureString "Password123@" -AsPlainText -Force
 
 # prompts for user details
 $FirstName = Read-Host "Enter First Name"
 $LastName = Read-Host "Enter Last Name"
-$Office = Read-Host "Enter Office"
+$Office = Read-Host "Enter Physical Delivery Office Name"
+$Address = Read-Host "Enter Street Address"
+$PostalCode = Read-Host "Enter Postal Code"
 $Telephone = Read-Host "Enter Telephone Number"
 $Email = Read-Host "Enter Email Address"
 $MailNickname = Read-Host "Enter Mail Nickname"
@@ -19,15 +22,16 @@ $GroupsInput = Read-Host "Enter comma-separated Security Groups (if any, leave b
 # produce derived values
 $username = "$($FirstName.Substring(0,1))$LastName".ToLower()
 $userPrincipalName = "$username@$domainname"
+$fullName = "$FirstName $LastName"
 
-# creates the new AD user
+# creates the new AD user in the default OU (paradigmcos.local\Users)
 New-ADUser `
-    -Name "$FirstName $LastName" `
+    -Name $fullName `
     -GivenName $FirstName `
     -Surname $LastName `
     -SamAccountName $username `
     -UserPrincipalName $userPrincipalName `
-    -Path "OU=Users,DC=domain,DC=com" ` #adjust accordingly
+    -Path "OU=Users,DC=paradigmcos,DC=local" ` # adjust accordingly if needed
     -AccountPassword $defaultpassword `
     -Enabled $true `
     -PassThru
@@ -42,13 +46,16 @@ if ($GroupsInput) {
 
 # sets the additional user properties for new user
 Set-ADUser $username `
-    -Office $Office `
-    -OfficePhone $Telephone `
-    -EmailAddress $Email `
     -Replace @{
-        mailNickname   = $MailNickname
-        proxyAddresses = ($ProxyAddressesIn -split ",")
-        title          = $JobTitle
-        department     = $Department
-        company        = $Company
+        physicalDeliveryOfficeName = $Office
+        streetAddress              = $Address
+        postalCode                 = $PostalCode
+        telephoneNumber            = $Telephone
+        mail                       = $Email
+        mailNickname               = $MailNickname
+        proxyAddresses             = ($ProxyAddressesIn -split ",")
+        title                      = $JobTitle
+        department                 = $Department
+        company                    = $Company
+        name                       = $fullName
     }
