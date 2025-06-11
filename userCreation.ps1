4772d9f3bbc210b92d893efe5cea6df2aae63547
001 <152649123+theredplanetsings@users.noreply.github.com>
2025-06-11 15:35:11 -0400

    Update userCreation.ps1

--- userCreation.ps1
+++ userCreation.ps1
@@ -1,41 +1,39 @@
 Import-Module ActiveDirectory
 #global constants
-$domainname = "paradigmcos.local"
-$company = "Paradigm Services" #edit as needed
+$domainname = "paradigmcos.com"
+$company = "Paradigm Companies"
 $defaultpassword = ConvertTo-SecureString "Password123@" -AsPlainText -Force
-$mailaddy = "jdoe@paradigmcos.com"
-# step 1: creating a new user in the AD [placeholders used for properties until actual values are known]
-New-ADUser `
-    -Name "John Doe" `
-    -GivenName "John" `
-    -Surname "Doe" `
-    -sAMAccountName "jdoe" `
-    -mailNickname "jdoe" `
-    -mail $mailaddy `
-    -UserPrincipalName "jdoe@paradigmcos.com" `
-    -Path "OU=Users,DC=paradigmcos,DC=local" ` # adjust accordingly if needed
-    -AccountPassword $defaultpassword `
-    -Enabled $true `
-    -PassThru
+$newUsername = "jdoe"
 
-$proxyaddy = "SMTP:$mailaddy"
+# step 1: creating a new user in the AD [placeholders used for properties until actual values are known]
+New-ADUser  `
+    -Name "John Doe" `
+    -AccountPassword $defaultpassword `
+    -GivenName "John" `
+    -Surname "Doe"  `
+    -SamAccountName $newUsername `
+    -UserPrincipalName "$newUsername@$domainname" `
+    -Path "OU=Internal,OU=Users,OU=PDC-SERVICES,DC=paradigmcos,DC=local" `
+    -Enabled $true
+    #-PassThru
+
 # step 2: assigning specified security groups to the user [currently using placeholders until actual property names are known]
-Add-ADGroupMember -Identity "IT Staff" -Members "jdoe"
+Add-ADGroupMember -Identity "VPN Users" -Members "$newUsername"
 # if we want to unassign a security group from a user:
-# Remove-ADGroupMember -Identity "IT Staff" -Members "jdoe" -Confirm:$false
+# Remove-ADGroupMember -Identity "IT-Security" -Members "jdoe" -Confirm:$false
 
 # step 3: assign additional user properties [currently using placeholders until actual property names are known]
-Set-ADUser $username `
+Set-ADUser `
+    -Identity "$newUsername" `
+    -Office "Main Office" `
+    -OfficePhone "123-456-7890" `
+    -EmailAddress "$newUsername@$domainname" `
     -Replace @{
-        physicalDeliveryOfficeName = "Corporate"
-        streetAddress              = "123 Jane Doe Lane"
-        postalCode                 = "12345"
-        telephoneNumber            = "123-456-7891"
-        mailNickname               = "jdoe"
-        proxyAddresses             = $proxyaddy
-        title                      = "IT Staff"
-        department                 = "Admin"
-        company                    = $company
-        st                         = "VA"
+        mailNickname = "jdoe"
+        proxyAddresses = "smtp:$newUsername@$domainname"
+        title = "IT Specialist"
+        department = "IT"
+        company = "$company"
     }
+
 # modify variables & attributes as needed for further testing
